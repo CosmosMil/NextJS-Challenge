2b25f29def31288a3a024c64eaea26440d4d88ee
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-06-06 13:12:05 +0200

    changes in colors

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -25,10 +25,10 @@ const Characters = ({ data }: InferGetServerSidePropsType<typeof getServerSidePr
             <React.Fragment key={character.id}>
 
 
-              <div className='h-80 w-54 p-3 border-2 border-indigo-400 overflow-hidden mx-2 my-2'
+              <div className='h-80 w-54 p-3 border-2 border-indigo-700 overflow-hidden mx-2 my-2'
               >
                 <img src={character.image} className='object-scale-down w-52 h-52' />
-                <div className='text-center text-indigo-600 p-2'>
+                <div className='text-center text-cyan-600 p-2'>
                   {character.name} <br /> {character.species} <br /> {character.status}
                 </div>
               </div>
