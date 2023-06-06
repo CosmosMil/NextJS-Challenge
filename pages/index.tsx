5d0312787a3853f90ddc1aca95b2ecedb98b97e4
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-06-06 13:23:38 +0200

    image added on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import image from '../images/FU.png'
 
 
 const Home: NextPage = () => {
   return (
-
     <div>
+      <Image src={image} alt='Rick and Morty say FU' />
 
     </div>
 
