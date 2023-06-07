f2cf407fb6fb82b5d0b1577368cacfdbbf058b67
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-06-07 13:03:29 +0200

    added an arrow link on the navbar, only displayed when on single character page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,17 +5,26 @@ import React from 'react'
 const Navbar = () => {
   const router = useRouter();
   const { pathname } = router;
+
+
   return (
     <nav>
       <div className='flex justify-start ml-9 my-3 '>
         {pathname === '/' ? (
           <Link className='p-1 rounded hover:bg-green-300  text-cyan-600' href='/characters'>CHARACTERS</Link >
         ) : (
-          <Link className='p-1 rounded hover:bg-green-300  text-cyan-600' href='/'>HOME</Link >
+          <>
+            {pathname === '/characters' ? (
+              <Link className='p-1 rounded hover:bg-green-300  text-cyan-600' href='/'>HOME</Link >) :
 
+
+              (
+                <Link href='/characters'>
+                  <div className='p-1 rounded hover:bg-green-300  text-cyan-600'>←</div>
+                </Link>)}
+
+          </>
         )}
-
-
 
       </div>
     </nav>
