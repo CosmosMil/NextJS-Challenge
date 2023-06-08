c8bfbf5bfac3ed83c4d085c15fc5bb8fd0aabff6
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-06-08 14:26:32 +0200

    added episodes page and episodes link on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,12 +10,20 @@ const Navbar = () => {
   return (
     <nav>
       <div className='flex justify-start ml-9 my-3 '>
-        {pathname === '/' ? (
+        {pathname === '/' ? (<>
           <Link className='p-1 rounded hover:bg-lime-300  text-cyan-600 text-2xl' style={{ fontFamily: 'get_schwifty' }} href='/characters'>CHARACTERS</Link >
+          <Link href='/episodes'>
+            <div className='p-1 rounded ml-9 hover:bg-lime-300 text-cyan-600 text-2xl' style={{ fontFamily: 'get_schwifty' }}>EPISODES</div>
+          </Link></>
+
         ) : (
           <>
-            {pathname === '/characters' ? (
-              <Link className='p-1 rounded hover:bg-lime-300 text-cyan-600 text-2xl' style={{ fontFamily: 'get_schwifty' }} href='/'>HOME</Link >) :
+            {pathname === '/characters' ? (<>
+              <Link className='p-1 rounded hover:bg-lime-300 text-cyan-600 text-2xl' style={{ fontFamily: 'get_schwifty' }} href='/'>HOME</Link >
+              <Link href='/episodes'>
+                <div className='p-1 rounded ml-9 hover:bg-lime-300 text-cyan-600 text-2xl' style={{ fontFamily: 'get_schwifty' }}>EPISODES</div>
+              </Link></>
+            ) :
 
 
               (
@@ -25,6 +33,7 @@ const Navbar = () => {
 
           </>
         )}
+
 
       </div>
     </nav>
---
+++ pages/episodes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Episodes = (props: Props) => {
+  return (
+    <div>EPISODES</div>
+  )
+}
+
+export default Episodes
