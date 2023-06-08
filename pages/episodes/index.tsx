d008e9c42e86b22b7090672d487c4f886c30a78f
CosmosMil <107584409+CosmosMil@users.noreply.github.com>
2023-06-08 15:07:47 +0200

    graphql query for season 1 episodes

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { resourceLimits } from 'worker_threads';
 
 type Props = {}
 
 const Episodes = (props: Props) => {
+
+
+
+
+  fetch('https://rickandmortyapi.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `query GetSeasonOne {
+        episodes(filter: {episode: "S01"})
+  {
+    results {
+      id
+      name
+      air_date
+      episode
+      characters {
+        name
+      }
+    }
+   }
+  
+}
+      `,
+    }),
+  })
+    .then((res) => res.json())
+    .then((result) => console.log(result));
+
   return (
-    <div>EPISODES</div>
+    <div>
+
+    </div>
   )
 }
 
